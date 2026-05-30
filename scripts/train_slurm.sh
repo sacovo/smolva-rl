f6d16bce75b841be697304c333f07dc88c4efd8d
Sandro Covo <sandro@sandrocovo.ch>
2026-05-31 01:16:05 +0200

    Disable shm for nccl

--- scripts/train_slurm.sh
+++ scripts/train_slurm.sh
@@ -22,8 +22,9 @@ export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
 export PYTHONIOENCODING=utf-8
 
-# Resolve NCCL Peer-to-Peer and InfiniBand communication failures on cluster nodes
+# Resolve NCCL Peer-to-Peer, shared memory, and InfiniBand communication failures on cluster nodes
 export NCCL_P2P_DISABLE=1
+export NCCL_SHM_DISABLE=1
 export NCCL_IB_DISABLE=1
 export NCCL_SOCKET_IFNAME=lo
 export GLOO_SOCKET_IFNAME=lo
